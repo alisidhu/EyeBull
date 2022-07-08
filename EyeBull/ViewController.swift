fb70407a8459011ae85f961b4de27fbf52f8b8f5
Ali <aliisidhu@gmail.com>
2022-07-08 17:28:33 +0500

    updating table for target value and staring new round

--- EyeBull/ViewController.swift
+++ EyeBull/ViewController.swift
@@ -8,28 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentSliderValue : Int = 50
+    @IBOutlet var slider: UISlider! // First press ctrl+ click on slider and select New Referencing Outlet then it will be connected to slider
+    var currentSliderValue : Int = 0
+    
+    @IBOutlet var targetLabel: UILabel!
+    var targetValue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //setting inital value of slider
+        currentSliderValue = lroundf(slider.value)
+        
+       setTargetRandomValue()
+
     }
 
 
     @IBAction func showAlert(){
+        let message = "The value of the slider is: \(currentSliderValue)" +
+                       "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(
             title: "Hello World!",
-            message: "The value of the slider is: \(currentSliderValue)" ,
+            message: message ,
             preferredStyle: .alert)
         let action = UIAlertAction(
             title: "Awosome", style: .default, handler: nil
         )
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startOverGame()
     }
     
     @IBAction func sliderOnMoved(_ slider : UISlider){
         currentSliderValue = lroundf(slider.value)
         print("The value of the slider is now: \(slider.value)")
+    }
+    
+     func startOverGame(){
+        setTargetRandomValue()
+        currentSliderValue = 50
+        slider.value = Float(currentSliderValue)
+    }
+    
+    func setTargetRandomValue(){
+        //Generating random number
+        targetValue = Int.random(in: 1...100)
+        updateLabels()
+    }
+    func updateLabels() {
+      targetLabel.text = String(targetValue)
     }
 }
 
