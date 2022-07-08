377ce4fce460dccdbb48372ed57eabe58beb685b
Ali <aliisidhu@gmail.com>
2022-07-08 16:37:57 +0500

    UI Added, Slider setup

--- EyeBull/ViewController.swift
+++ EyeBull/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentSliderValue : Int = 50
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func showAlert(){
+        let alert = UIAlertController(
+            title: "Hello World!",
+            message: "The value of the slider is: \(currentSliderValue)" ,
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: "Awosome", style: .default, handler: nil
+        )
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderOnMoved(_ slider : UISlider){
+        currentSliderValue = lroundf(slider.value)
+        print("The value of the slider is now: \(slider.value)")
+    }
 }
 
